e9ff24f5cc0a1ff321cf2aa1bc561d07c5285467
Femi Obadimu <ficazzosam@gmail.com>
2026-05-04 19:17:21 +0100

    Store Changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.bptp9nm5go8"
+    "revision": "0.5kqk7rs7oko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
