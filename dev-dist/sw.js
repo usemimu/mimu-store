6f66664a0fc5a2f60e8be1f9e622561a4b34e01d
Femi Obadimu <ficazzosam@gmail.com>
2026-05-06 11:40:35 +0100

    Testing Phase

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.5kqk7rs7oko"
+    "revision": "0.u4guqdf0gi"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
